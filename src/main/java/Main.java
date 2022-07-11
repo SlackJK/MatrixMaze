f1c9f4e4d0d96b7a5a1a62979bc61efba93fab90
SlackJK <87824853+SlackJK@users.noreply.github.com>
2022-07-11 14:59:37 +0200

    Cleaning.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,6 @@ public class Main
     public static Input I = new Input();
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
 
-
-        String testin =
-                "........\n" +
-                "..S..#..\n" +
-                ".....#X.\n" +
-                "........\n";
-
-
         Lee L = new Lee();
         I.PickInput();
         System.out.println("The shortest maze path through your maze is:");
@@ -29,29 +21,6 @@ public class Main
         else{
             Main.main(null);
         }
-/*
-        InputParser MH = new InputParser("S","X");
-        //ArrayList<ArrayList<Integer>> mat = MH.InputToMatrix(testin,"\n");
-        Lee z = new Lee();
-        //ArrayList<String> x =z.alg(mat,MH.SourceChords,MH.TargetChords);
-        //System.out.println(x);
-        /*
-        Chords S = new MainHelp().Chord(test2d,"S");
-        Chords T = new MainHelp().Chord(test2d,"X");
-        System.out.println(test2d);
-        test2d.get(S.y).set(0,"0");
-        test2d.get(T.y).set(T.x,"0");
-        //String temp[][] = test2d.stream().map(l -> l.stream().toArray(Integer[]::new)).toArray(String[][]::new);
-        int matrix[][] = test2d.stream().map(l -> l.stream().mapToInt(Integer::parseInt)).toArray(int[][]::new);
-        for (int i = 0; i < matrix.length; i++)
-        {
-            for (int j = 0; j < matrix[0].length; j++)
-            {
-                System.out.println(matrix[i][j]);
-            }
-            System.out.println("\n");
-        }
 
-         */
     }
 }
